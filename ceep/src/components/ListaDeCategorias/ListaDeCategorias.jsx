11dce7f788a7ceb0019908b3f75fc6df333b98f2
GUIFRE88 <guilherme.fre073@gmail.com>
2021-03-16 07:07:09 -0300

    Criado Observable.

--- ceep/src/components/ListaDeCategorias/ListaDeCategorias.jsx
+++ ceep/src/components/ListaDeCategorias/ListaDeCategorias.jsx
@@ -2,8 +2,18 @@ import React, { Component } from 'react'
 import "./estilo.css"
 
 class ListaDeCategorias extends Component {
+    // Método chamado automaticamente após a criação de todos os compomentes.
+    componentDidMount(){
+        this.props.categorias.inscrever(this._novasCategorias.bind(this));
+    }
+
+    _novasCategorias(categorias){
+        console.log(categorias)
+    }
+
     _handleEventoInput(e){
-        if(e.key == "Enter"){
+        console.log('teste')
+        if(e.key === "Enter"){
             let valorCategoria = e.target.value
             this.props.adicionarCategoria(valorCategoria)
         }
@@ -12,7 +22,7 @@ class ListaDeCategorias extends Component {
         return ( 
         <section className="lista-categorias">
             <ul className="lista-categorias_lista">
-                {this.props.categorias.map((categoria, index)=>{
+                {this.props.categorias.categorias.map((categoria, index)=>{
                     return <li key={index} className="lista-categorias_item">{categoria}</li>;
                 })}
             </ul>
